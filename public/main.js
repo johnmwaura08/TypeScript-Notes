3daff691f82385e9660c6b50e60e5b00aecef866
John Mwaura <johnmwaura08@gmail.com>
2020-09-10 13:30:45 -0500

    using type aliases to avoid code duplication

--- public/main.js
+++ public/main.js
@@ -1,25 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// let greetings = () => {
-//   console.log("hey there");
-// }
-//typescript will automatically infer greetings to be of type function 
-// we can initialize greetings to be a function with capital F
-var greetings;
-greetings = function () {
-    console.log("hello again");
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-// we can also give types to parameters in a function
-var add = function (a, b, c) {
-    console.log(a + b);
-    console.log(c); //logs undefined at the moment
+var greet = function (user) {
+    console.log(user.name + " says hello");
 };
-add(5, 6); //logs 11 in the console
-// add("6", 7) //arleady shows an error because a is supposed to be a number
-// if you want an optional parameter just put a question mark after it and only put it after the required parameters
-var minus = function (a, b) {
-    return a - b;
-};
-var result = minus(10, 7); // the type of result is inferred from the returns of minus
-// however we can explicitly define the type of the return statement by putting after the brackets that contain the parameters
-// if we dont return anything like our add function we can give it type void because it doesnt return anything 
+// this type aliases help us out alot and reduce code duplication
--- src/main.ts
+++ src/main.ts
@@ -9,43 +9,21 @@ To achieve this, simply type export on the top of your script or additionally yo
 */
 export {}
 
-// let greetings = () => {
+/*----Type Alias */
 
-//   console.log("hey there");
-  
-// }
-//typescript will automatically infer greetings to be of type function 
-// we can initialize greetings to be a function with capital F
+type StringOrNum = string | number; // saves us time instead of writing string or number everywhere
 
-let greetings : Function;
-
-greetings = () => {
-  console.log("hello again");
-  
+type objWithName = {
+  name : string,
+  uid: StringOrNum
+}
+const logDetails = (uid: StringOrNum, item : string) => {
+  console.log(`${item} has a uid of ${uid}`)
 }
 
-// we can also give types to parameters in a function
-
-const add = ( a : number, b: number, c? : number| string) : void  => {
-
-  console.log(a + b); 
-  console.log(c); //logs undefined at the moment
+const greet = (user : objWithName) => {
+  console.log(`${user.name} says hello`);
   
 }
 
-add( 5, 6) //logs 11 in the console
-
-// add("6", 7) //arleady shows an error because a is supposed to be a number
-
-// if you want an optional parameter just put a question mark after it and only put it after the required parameters
-
-
-const minus = ( a: number, b: number) : number => {
-  return a - b;
-
-}
-  let result = minus (10, 7) // the type of result is inferred from the returns of minus
-
-  // however we can explicitly define the type of the return statement by putting after the brackets that contain the parameters
-
-  // if we dont return anything like our add function we can give it type void because it doesnt return anything 
+// this type aliases help us out alot and reduce code duplication
