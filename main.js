dd355f8c523bec19923adfc3fc033fabfbf463c5
John Mwaura <johnmwaura08@gmail.com>
2020-09-10 11:05:36 -0500

    arrays and objects basics typescript

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-//typescript uses strict types while javascript doesnt
-// we can change the values but not the types
-// typescript infers the type based on the value we assign it
-var character = 'jone'; // typescript infers that this variable is of the type string
-// myName = 40 // theres an error because i cant assign myname to type thats not string 
-var circumference = function (diameter) {
-    return diameter * Math.PI;
+//arrays
+var names = ['juliette', 'jennifer', 'katelyn'];
+names.push('steve');
+// when we declare an array, the types that we assigned cant be changed later on
+// we can have a mixed array if we declare it to have mixed types 
+//objects 
+var ninja = {
+    name: 'jon',
+    belt: 'blue',
+    age: 22
 };
-console.log(circumference("hey")); // we'll return Nan if we dont define the type 
-console.log(circumference(7)); // we'll return 21.991148575128552 which is the circumference .. this helps us catch errors 
-// it wont complie to javascript if the wrong type is sent in
-//
+ninja.age = 40; // this is fine
+// ninja.age = '44' // this is an error because the type is arleady a number 
--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
+//arrays
 
-//typescript uses strict types while javascript doesnt
+let names = ['juliette', 'jennifer', 'katelyn'];
 
-// we can change the values but not the types
-// typescript infers the type based on the value we assign it
+names.push('steve')
 
-let character = 'jone';  // typescript infers that this variable is of the type string
+// when we declare an array, the types that we assigned cant be changed later on
 
-// myName = 40 // theres an error because i cant assign myname to type thats not string 
-
-
-const circumference = (diameter : number) => {
-    return diameter * Math.PI
-}
+// we can have a mixed array if we declare it to have mixed types 
 
 
-console.log(circumference("hey"))  // we'll return Nan if we dont define the type 
-console.log(circumference(7))  // we'll return 21.991148575128552 which is the circumference .. this helps us catch errors 
-// it wont complie to javascript if the wrong type is sent in
-//
+//objects 
+
+
+let ninja = {
+
+    name : 'jon',
+    belt: 'blue',
+    age: 22
+};
+
+ninja.age = 40 // this is fine
+// ninja.age = '44' // this is an error because the type is arleady a number 
