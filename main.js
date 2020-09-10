56bbe7867759795700b4cef4a7a77a9a52d8b277
John Mwaura <johnmwaura08@gmail.com>
2020-09-10 11:30:13 -0500

    using explicit types and union types

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-//arrays
-var names = ['juliette', 'jennifer', 'katelyn'];
-names.push('steve');
-// when we declare an array, the types that we assigned cant be changed later on
-// we can have a mixed array if we declare it to have mixed types 
-//objects 
-var ninja = {
-    name: 'jon',
-    belt: 'blue',
-    age: 22
-};
-ninja.age = 40; // this is fine
-// ninja.age = '44' // this is an error because the type is arleady a number 
+"use strict";
+exports.__esModule = true;
+/*------------------explicit types------------------ */
+var character; // this lets us initialize a value and in future only allow a string
+var age; //this lets us initialize a value and in future only allow a nuber
+var isLoggedIn; //this lets us initialize a value and in future only allow a boolean
+// age = '44' // this is an error because i'm trying to give it a string and it wont accept
+age = 34; // this is okay
+character = 'superman'; //okay
+isLoggedIn = true; // okay
+/* ............arrays......... */
+var ninjas = []; // we've initialized this with an empty array and in future we can only push strings into the array
+/***union types---*/
+//several different types
+var mixed = [];
+mixed.push('hello'); // okay
+mixed.push(34); // okay
+console.log(mixed); //returns [ 'hello' , 34]
+// wwe can also nion types on normal variables not just arrays
+var uid;
+uid = '123'; // this is okay
+uid = 345; // this is okay 
+// uid = false // an error because false is a boolean type 
+/*---Objects---------- */
+var ninjaOne;
+ninjaOne = { name: 'jonte', age: 22 }; // this is okay
+// however you can be explicit with the types of the object properties before giving them a value
+var ninjaTwo;
+ninjaTwo = { name: 'james', age: 23, belt: 'black' };
--- main.ts
+++ main.ts
@@ -1,23 +1,70 @@
-//arrays
 
-let names = ['juliette', 'jennifer', 'katelyn'];
+/*Typescript is moduler and each module has it’s own block.
+ So, basically if you could somehow enclose the variable which is declared as let within it’s own module,
+  the error will be resolved because now your let variable is in it’s separate block.
 
-names.push('steve')
+To achieve this, simply type export on the top of your script or additionally you can type export {}.
+ Now you will see that the error is resolved.
 
-// when we declare an array, the types that we assigned cant be changed later on
-
-// we can have a mixed array if we declare it to have mixed types 
-
-
-//objects 
+*/
+export {}
 
 
-let ninja = {
 
-    name : 'jon',
-    belt: 'blue',
-    age: 22
-};
+/*------------------explicit types------------------ */
 
-ninja.age = 40 // this is fine
-// ninja.age = '44' // this is an error because the type is arleady a number 
+let character : string ; // this lets us initialize a value and in future only allow a string
+let age : number; //this lets us initialize a value and in future only allow a nuber
+let isLoggedIn: boolean; //this lets us initialize a value and in future only allow a boolean
+
+
+// age = '44' // this is an error because i'm trying to give it a string and it wont accept
+age = 34; // this is okay
+character = 'superman'; //okay
+isLoggedIn = true // okay
+
+
+/* ............arrays......... */
+
+let ninjas : string [] = [] // we've initialized this with an empty array and in future we can only push strings into the array
+
+
+/***union types---*/
+
+//several different types
+
+let mixed : (string|number)[] = [];
+
+mixed.push('hello') // okay
+mixed.push(34) // okay
+
+console.log(mixed);//returns [ 'hello' , 34]
+
+
+ // wwe can also nion types on normal variables not just arrays
+
+let uid : string|number;
+
+uid = '123' // this is okay
+
+uid = 345 // this is okay 
+
+// uid = false // an error because false is a boolean type 
+
+
+/*---Objects---------- */
+
+let ninjaOne : object;
+
+ninjaOne = {name : 'jonte', age : 22} // this is okay
+
+// however you can be explicit with the types of the object properties before giving them a value
+
+let ninjaTwo : {
+
+    name : string,
+    age : number,
+    belt : string
+}
+
+ninjaTwo = { name : 'james', age : 23, belt: 'black'}
